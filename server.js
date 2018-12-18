1c160b1f0be7046353283d1bcd1bbd65d0e81bc4
asakusuma <asa.kusuma@gmail.com>
2018-12-17 17:22:14 -0800

    Support turning off clear-site-data

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ module.exports = function() {
 
   let clearSiteDataFlag = false;
 
-  app.use(function(req, res, next) {
+  app.use(function(_req, res, next) {
     if (clearSiteDataFlag) {
       res.setHeader('Clear-Site-Data', ['"storage"'])
     }
@@ -14,10 +14,17 @@ module.exports = function() {
 
   app.use(express.static('static'));
 
-  app.get('/clearSiteDataOn', function(req, res) {
+  app.get('/clearSiteDataOn', function(_req, res) {
     clearSiteDataFlag = true;
-    res.send('Clear-Site-Data: "storage", "executionContexts"');
+    res.send('Turing on clear site data');
   });
+
+  app.get('/clearSiteDataOff', function(_req, res) {
+    clearSiteDataFlag = false;
+    res.send('Turning off clear site data');
+  });
+
+
 
   app.listen(port, () => console.log(`Clear-Site-Data test app running on port ${port}!`))
   return {
